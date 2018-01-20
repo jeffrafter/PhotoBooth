223f3cd660ab48ac5ed21019b80d1b9e3e7935aa
jeffrafter <jeffrafter@gmail.com>
2018-01-19 19:54:03 -0800

    Initial

---
+++ PhotoBooth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoBooth
+//
+//  Created by Jeff Rafter on 1/19/18.
+//  Copyright © 2018 Rplcat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
